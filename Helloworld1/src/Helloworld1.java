fe4980bbcf8d73efbddbe283384881e935337ca3
ameenasamra1@gmail.com <ameenasamra1@gmail.com>
2017-06-05 17:36:46 -0700

    This is a Java introductory program to print Hello java.

---
+++ Helloworld1/src/Helloworld1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Amina
+ *
+ */
+public class Helloworld1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
