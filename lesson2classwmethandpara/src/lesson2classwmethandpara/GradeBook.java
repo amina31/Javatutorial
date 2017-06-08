77d56319d83eeae87c8038c605691743a5895795
ameenasamra1@gmail.com <ameenasamra1@gmail.com>
2017-06-07 23:53:08 -0700

    program has a class with a parameter and displays a message on screen.

---
+++ lesson2classwmethandpara/src/lesson2classwmethandpara/GradeBook.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package lesson2classwmethandpara;
+
+//Class declaration with a method that has a parameter.
+
+public class GradeBook
+{
+   // display a welcome message to the GradeBook user
+   public void displayMessage( String courseName )
+   {
+      System.out.printf( "Welcome to this grade book for\n%s!\n", 
+         courseName );
+   } // end method displayMessage
+
+} // end class GradeBook
+
