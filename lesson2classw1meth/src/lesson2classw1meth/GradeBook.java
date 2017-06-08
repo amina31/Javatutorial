80aa73e494f9cc56d0f28eb1f31582eab59e5616
ameenasamra1@gmail.com <ameenasamra1@gmail.com>
2017-06-07 23:31:19 -0700

    program displays welcome msg.This is Class with one method.

---
+++ lesson2classw1meth/src/lesson2classw1meth/GradeBook.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lesson2classw1meth;
+
+
+
+
+	//  here Class declaration is  with one method.
+
+	public class GradeBook
+	{
+	   //  It display a welcome message to the GradeBook user
+	   public void displayMessage()
+	   {
+	      System.out.println( "Welcome to the Grade Book!" );
+	   } // end method displayMessage
+
+	} // end class GradeBook
+
---
+++ lesson2classw1meth/src/lesson2classw1meth/GradeBookTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lesson2classw1meth;
+
+
+// it  Creates  a GradeBook object and calls  its displayMessage method.
+
+public class GradeBookTest
+{
+       // main method begins program execution
+   public static void main( String args[] )
+{ 
+   // create a GradeBook object and assign it to myGradeBook
+   GradeBook myGradeBook = new GradeBook(); 
+
+   // call myGradeBook's displayMessage method
+   myGradeBook.displayMessage(); 
+} // end main
+
+} // end class GradeBookTest
+
