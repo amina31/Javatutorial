15d0695202ba2de8224dafe53ed4e23e858ebc76
ameenasamra1@gmail.com <ameenasamra1@gmail.com>
2017-06-11 00:11:08 -0700

    Program uses do while loop to display a list of 10 numbers.

---
+++ lesson4dowhile/src/DoWhile.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Amina
+ *
+ */
+public class DoWhile {
+
+	// do...while repetition statement.
+
+	public class DoWhileTest 
+	{  
+	   public static void main( String args[] )
+	   {
+	      int counter = 1; // initialize counter
+
+	      do 
+	      {
+	         System.out.printf( "%d  ", counter );
+	         ++counter;
+	      } while ( counter <= 10 ); // end do...while 
+
+	      System.out.println(); // outputs a newline
+	   } // end main
+	} // end class DoWhileTest
---
+++ lesson4dowhile/src/lesson4dowhile/DoWhile.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package lesson4dowhile;
+
+
+	// do...while repetition statement.
+
+	public class DoWhile 
+	{  
+	   public static void main( String args[] )
+	   {
+	      int counter = 1; // initialize counter
+
+	      do 
+	      {
+	         System.out.printf( "%d  ", counter );
+	         ++counter;
+	      } while ( counter <= 10 ); // end do...while 
+
+	      System.out.println(); // outputs a newline
+	   } // end main
+	} // end class DoWhileTest
+
