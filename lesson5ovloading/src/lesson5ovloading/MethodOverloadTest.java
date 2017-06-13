15686afc35774863395a31cb84e1409dbf57ae2f
ameenasamra1@gmail.com <ameenasamra1@gmail.com>
2017-06-13 16:30:02 -0700

    program for method overloading.

---
+++ lesson5ovloading/src/lesson5ovloading/MethodOverloadTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package lesson5ovloading;
+
+//Application to test class MethodOverload.
+
+public class MethodOverloadTest
+{
+public static void main( String args[] ) 
+{
+   MethodOverload methodOverload = new MethodOverload();
+   methodOverload.testOverloadedMethods();
+} // end main
+} // end class MethodOverloadTest
